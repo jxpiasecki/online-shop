fd9cfb340f041da0773391891b5d00a15f620a04
Jarosław Piasecki <jarek.piasecki@hrlink.pl>
2020-12-29 20:37:02 +0100

    Add and handle cart items dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,12 +3,14 @@ import {Link, Route, Switch} from 'react-router-dom';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import SignInAndSignUpPage from "../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import {auth} from "../../firebase/firebase.utils";
 
 import {connect} from 'react-redux';
 import userReducer from "../../redux/user/user.reducer";
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, toggleCartHidden}) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo"/>
@@ -21,12 +23,15 @@ const Header = ({currentUser}) => (
                     ? <div className="option" onClick={() => auth.signOut()}>Sign out</div>
                     : <Link className="option" to="/sign-in">Sign in/up</Link>
             }
+            <CartIcon />
         </div>
+        {toggleCartHidden ? '' : <CartDropdown />}
     </div>
 )
 
 const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+    currentUser: state.user.currentUser,
+    toggleCartHidden: state.cart.hidden
 })
 
 export default connect(mapStateToProps)(Header);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,6 @@
-import {UserAtionTypes} from "./user.types";
+import {UserActionTypes} from "./user.types";
 
 export const setCurrentUser = user => ({
-    type: UserAtionTypes.SET_CURRENT_USER,
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 })
